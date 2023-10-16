bc4d8eff0cdb7c16005fecb5fa74c7f8782ecdf5
C.Mirkin <ciromirkin@gmail.com>
2023-10-16 15:56:16 -0300

    change: event of the reset btn

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,29 @@ const iconList = icons;
 showGameBoard(iconList);
 
 const resetBtn = document.getElementById("reset-btn")!;
-resetBtn.addEventListener('click', () => {
+resetBtn.addEventListener('click', (e) => {
+    const target = e.target as HTMLElement;
+    const actionName: string = target.parentElement?.getAttribute('action') || "";
+    if(!!actionName) {
+        actionList.forEach(action => {
+            if(action.name == actionName) action.do()
+        });
+    }
+})
+
+interface action {
+    name: string,
+    do: Function
+}
+
+const actionList: Array<action> = [
+    {
+        name: "reset",
+        do: () => resetGameBoard()
+    }
+]
+
+const resetGameBoard = () => {
     resetGameBoardController();
     showGameBoard(iconList);
-})
+}
