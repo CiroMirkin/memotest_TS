562ea2d7392ba439d619cc20e4eac4ad238bcfef
C.Mirkin <ciromirkin@gmail.com>
2023-10-16 17:24:43 -0300

    change: index icons from png to svg

--- src/icons.ts
+++ src/icons.ts
@@ -6,46 +6,46 @@ export interface icon {
 export const icons: Array<icon> = [ 
     {
       pairID: "1",
-      src: "icons/pic (1).png",
+      src: "icons/pic (1).svg",
     },
     {
       pairID: "2",
-      src: "icons/pic (2).png",
+      src: "icons/pic (2).svg",
     },
     {
       pairID: "3",
-      src: "icons/pic (3).png",
+      src: "icons/pic (3).svg",
     },
     {
       pairID: "4",
-      src: "icons/pic (4).png",
+      src: "icons/pic (4).svg",
     },
     {
       pairID: "5",
-      src: "icons/pic (5).png",
+      src: "icons/pic (5).svg",
     },
     {
       pairID: "6",
-      src: "icons/pic (6).png",
+      src: "icons/pic (6).svg",
     },
     
     {
       pairID: "7",
-      src: "icons/pic (7).png",
+      src: "icons/pic (7).svg",
     },
     
     {
       pairID: "8",
-      src: "icons/pic (8).png",
+      src: "icons/pic (8).svg",
     },
     
     {
       pairID: "9",
-      src: "icons/pic (9).png",
+      src: "icons/pic (9).svg",
     },
     
     {
       pairID: "10",
-      src: "icons/pic (10).png",
+      src: "icons/pic (10).svg",
     },
 ];
