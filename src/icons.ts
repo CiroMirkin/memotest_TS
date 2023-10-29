0c7aa18483764f101a4a62f2c6649efa1f981ca5
C.Mirkin <ciromirkin@gmail.com>
2023-10-29 15:50:17 -0300

    delete: array of icons in icons.ts file

--- src/icons.ts
+++ src/icons.ts
@@ -2,39 +2,3 @@ export interface icon {
   pairID: string,
   src: string
 }
-
-export const icons: Array<icon> = [
-  {
-    pairID: "1",
-    src: '<img xmlns="http://www.w3.org/1999/xhtml" id="bubble-chat-lineal-colored" loading="eager" imagename="Bubble Chat" src="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/64a2fa3ec53069b4f73eade1_VcGScP6QT8eLWda1biaT.svg" alt="Bubble Chat" class="illustrations_image" />'
-  },
-  {
-    pairID: "2",
-    src: '<img xmlns="http://www.w3.org/1999/xhtml" id="call-lineal-colored-3" loading="eager" imagename="Call" src="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/64a2fa0ac29db9b2da07d3ee_ikswccdHRtywfO6eSjF6.svg" alt="Call" class="illustrations_image" />'
-  },
-  {
-    pairID: "3",
-
-    src: '<img xmlns="http://www.w3.org/1999/xhtml" id="globe-earth-lineal-colored" loading="eager" imagename="Globe Earth" src="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/64a2fa3a301a6b9c317c705f_t0Fli6xpRum7TSXQDFB3.svg" alt="Globe Earth" class="illustrations_image" />'
-  },
-  {
-    pairID: "4",
-    src: '<img xmlns="http://www.w3.org/1999/xhtml" id="dustbin-lineal-colored" loading="eager" imagename="Dustbin" src="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/64a2fa31e3d11964e888dabe_lDw3RfBuQ3Sf6nRzCUaf.svg" alt="Dustbin" class="illustrations_image" />'
-  },
-  {
-    pairID: "5",
-    src: '<img xmlns="http://www.w3.org/1999/xhtml" id="dictionary-lineal-colored-a" loading="eager" imagename="Dictionary" src="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/64a2fa3e88f0a83348e33724_RaWYRPzPQC3CW9PrWhcg.svg" alt="Dictionary" class="illustrations_image" />'
-  },
-  {
-    pairID: "7",
-    src: '<img xmlns="http://www.w3.org/1999/xhtml" id="spray-paint-lineal-colored" loading="eager" imagename="Spray Paint" src="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/64a2fa4dc8b2c6dc446994df_zeJWanWQHJewk5z5bng4.svg" alt="Spray Paint" class="illustrations_image" />'
-  },
-  {
-    pairID: "8",
-    src: '<img xmlns="http://www.w3.org/1999/xhtml" id="idea-lineal-colored-5" loading="eager" imagename="Idea" src="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/64a2f9f936adffb8a70bbef4_mrBLefJURvqadWQuvvoH.svg" alt="Idea" class="illustrations_image" />'
-  },
-  {
-    pairID: "9",
-    src: '<img xmlns="http://www.w3.org/1999/xhtml" id="chess-piece-lineal-colored-2" loading="eager" imagename="Chess Piece" src="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/64a2fa13942703bb5629346b_qp79ExtkRyKKMTBIMP9x.svg" alt="Chess Piece" class="illustrations_image" />'
-  },
-];
