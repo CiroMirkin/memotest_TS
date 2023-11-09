8aa4bc01d7313a63a7efb1831aa8403ce31986cc
c.mirkin <ciromirkin@gmail.com>
2023-11-09 10:41:53 -0300

    add: confetti

--- src/userWonSingView.ts
+++ src/userWonSingView.ts
@@ -1,13 +1,9 @@
+import confetti from 'canvas-confetti'
 
 const userWonSingElement = document.getElementById('the-user-won-sing')!;
 
-export const showUserWonSing = (winningColor?: string) => {
-    winningColor  = winningColor || "";
-    if(!!winningColor) {
-        userWonSingElement.classList.remove("user-won-sing--red");
-        userWonSingElement.classList.remove("user-won-sing--blue");
-        userWonSingElement.classList.add(`user-won-sing--${winningColor}`)
-    }
+export const showUserWonSing = () => {
+    confetti()
     userWonSingElement.classList.replace('user-won-sing--hide', 'user-won-sing--show')
 }
 export const hideUserWonSing = () => {
