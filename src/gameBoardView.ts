b5e2d641db6780eef96273d394f0ade47777d45c
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-06 02:06:49 -0300

    refactor: convert views to pure render functions

--- src/gameBoardView.ts
+++ src/gameBoardView.ts
@@ -1,37 +1,12 @@
-export interface picture {
-    id: string,
-    pairID: string,
-    src: string
-}
-  
-interface gameBoardView {
-    pictures: Array<picture>;
-    getCardElements(): HTMLElement | DocumentFragment;
-}
-  
-class GameBoardView implements gameBoardView {
-    pictures: Array<picture>;
-    constructor(pictures: Array<picture>) {
-      this.pictures = pictures
-    }
-    private convertPictureToHTMLElement(picture: picture): HTMLElement {
-      const pictureElement = document.createElement('li');
-      pictureElement.classList.add(...['card', 'card--no-selected']);
-      pictureElement.setAttribute('id', picture.id);
-      pictureElement.setAttribute('pairid', picture.pairID);
-      const pictureInPictureElement = document.createElement('div');
-      pictureInPictureElement.classList.add('card__img-container');
-      pictureInPictureElement.style.backgroundImage = `url(${picture.src})`;
-      pictureElement.appendChild(pictureInPictureElement);
-      return pictureElement;
-    }
-    getCardElements(): DocumentFragment {
-      const pictureListElement = document.createDocumentFragment();
-      this.pictures.forEach(picture =>
-        pictureListElement.appendChild(this.convertPictureToHTMLElement(picture))
-      );
-      return pictureListElement;
-    }
-}
+import { GameState } from './gameState';
+import { renderCard } from './cardView';
 
-export default GameBoardView;
+export const renderGameBoard = (state: GameState): DocumentFragment => {
+  const fragment = document.createDocumentFragment();
+  
+  state.cards.forEach(cardState => {
+    fragment.appendChild(renderCard(cardState));
+  });
+  
+  return fragment;
+};
